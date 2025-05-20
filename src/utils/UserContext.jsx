d4e0b5b58d727de0663dae740706e35dcc11fd1f
Veshal Raj <veshalraj1307@gmail.com>
2025-05-20 09:28:02 +0530

    add: dynamically adding sender email and password is done

--- src/utils/UserContext.jsx
+++ src/utils/UserContext.jsx
@@ -4,11 +4,12 @@ import { createContext, useState } from 'react';
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState({ id: null, email: null, name: null });
-  const [loading, setLoading] = useState(true);
+  const [ user, setUser ] = useState({ id: null, email: null, name: null });
+  const [ userVerified, setUserVerified ] = useState(false);
+  const [ loading, setLoading ] = useState(true);
 
   return (
-    <UserContext.Provider value={{ user, setUser, loading, setLoading }}>
+    <UserContext.Provider value={{ user, setUser, userVerified, setUserVerified, loading, setLoading }}>
       {children}
     </UserContext.Provider>
   );
